0a21f06a31c879e43443f0f52fb28cd5121e7f13
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-06-15 13:54:07 +0200

    update hero direction / create Map class

--- src/Map.java
+++ src/Map.java
@@ -1,5 +1,52 @@
 package src;
 
 public class Map {
+  private char[][] map;
 
+  // constrcuteur pour initialiser la carte
+  public Map() {
+    this.map = new char[][] {
+        "###    ######    ###".toCharArray(),
+        "###      ##      ###".toCharArray(),
+        "##     ##  ##     ##".toCharArray(),
+        "#      ##  ##      #".toCharArray(),
+        "##                ##".toCharArray(),
+        "#####          #####".toCharArray(),
+        "###### ##  ##  #####".toCharArray(),
+        " #     ######     # ".toCharArray(),
+        "     ########       ".toCharArray(),
+        "    ############    ".toCharArray(),
+        "    ############    ".toCharArray(),
+        "     ########      #".toCharArray(),
+        " #     ######     ##".toCharArray(),
+        "###### ##  ## ######".toCharArray(),
+        "#####          #####".toCharArray(),
+        "##                ##".toCharArray(),
+        "#   ## #    # ##   #".toCharArray(),
+        "##   ##      ##   ##".toCharArray(),
+        "###    #    #    ###".toCharArray(),
+        "###    ######    ###".toCharArray()
+    };
+  }
+
+  // Je défini la méthode afficherCarte()
+  public void displayMap() {
+    for (char[] row : map) {
+      System.out.println(new String(row));
+    }
+    System.out.println();
+  }
+
+  // Méthode pour vérifier si une position est valide sur la carte
+  public boolean isValidPosition(int x, int y) {
+    if (x < 0 || x >= map[0].length || y < 0 || y >= map.length || map[y][x] == '#') {
+      return false;
+    }
+    return true;
+  }
+
+  // méthode montrer la position du Hero
+  public void setPosition(int x, int y, char symbol) {
+    map[y][x] = symbol;
+  }
 }
