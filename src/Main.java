a588c630b34b0ce1b9548b2ef30143a2d0ca9543
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-06-15 14:02:27 +0200

    create instance hero2

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,38 @@ package src;
 
 public class Main {
   public static void main(String[] args) {
-    // instance de la map
-    Map map = new Map();
-    // instance de Hero
-    Hero hero = new Hero(map, 3, 0);
+    // instance de la map 1
+    Map map1 = new Map();
+    // instance de Hero 1
+    Hero hero1 = new Hero(map1, 3, 0);
 
-    // affiche la carte initiale
-    System.out.println("Initial map:");
-    map.displayMap();
+    // Affiche la carte initiale du Hero 1
+    System.out.println("Initial map for hero 1:");
+    map1.displayMap();
 
     String directions = "SSSSEEEEEENN";
 
-    // deplacement du Hero
-    hero.move(directions);
-    map.displayMap();
+    // deplacement du Hero 1
+    hero1.move(directions);
+    System.out.println("Map after movements for Hero 1:");
+    map1.displayMap();
+
+    // instance de la map 2
+    Map map2 = new Map();
+    // instance de Hero 2
+    Hero hero2 = new Hero(map2, 6, 9);
+
+    // affiche la carte initiale du hero 2
+    System.out.println("Initial map for hero 2:");
+    map2.displayMap();
+
+    String directions2 = "OONOOOSSO";
+
+    // Déplacement du hero 2 pour le deuxième test
+    hero2.move(directions2);
+
+    // Affiche la carte après les déplacements pour le deuxième test
+    System.out.println("Map after movements for Hero 2:");
+    map2.displayMap();
   }
 }
